59bd024785ba5456cbbfe265c3c1bed2c11bfa45
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-30 16:08:29 -0600

    Counter: Redirect to login page if unauthorized

--- src/app/counter/counter/counter.component.ts
+++ src/app/counter/counter/counter.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, OnDestroy, AfterViewInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { CounterAuthService } from "src/app/common/counter/service/counter-auth.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-counter",
@@ -6,9 +8,23 @@ import { Component, OnInit, OnDestroy, AfterViewInit } from "@angular/core";
   styleUrls: ["./counter.component.css"],
 })
 export class CounterComponent implements OnInit {
-  constructor() {}
+  authorized: boolean = false;
 
-  ngOnInit() {}
+  constructor(
+    private router: Router,
+    private authService: CounterAuthService
+  ) {}
+
+  ngOnInit() {
+    this.authService.checkAuth().subscribe(
+      () => (this.authorized = true),
+      (error) => {
+        if (![401, 403].includes(error.error.status))
+          alert("Error checking authorization: Status " + error.error.status);
+        this.router.navigate(["/counter/login"]);
+      }
+    );
+  }
 
   logOut() {
     localStorage.removeItem("token");
