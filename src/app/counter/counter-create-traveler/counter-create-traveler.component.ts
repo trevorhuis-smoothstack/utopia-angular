a1f42216948784198ac21926d3e5e913899b33d0
Justin O'Brien <justin.obrien@smoothstack.com>
2020-07-02 10:13:12 -0600

    Couter: Add form to create traveler component

--- src/app/counter/counter-create-traveler/counter-create-traveler.component.ts
+++ src/app/counter/counter-create-traveler/counter-create-traveler.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CounterHttpService } from "src/app/common/counter/service/counter-http.service";
+import { CounterDataService } from "src/app/common/counter/service/counter-data.service";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-counter-create-traveler',
-  templateUrl: './counter-create-traveler.component.html',
-  styleUrls: ['./counter-create-traveler.component.css']
+  selector: "app-counter-create-traveler",
+  templateUrl: "./counter-create-traveler.component.html",
+  styleUrls: ["./counter-create-traveler.component.css"],
 })
 export class CounterCreateTravelerComponent implements OnInit {
+  form = new FormGroup({
+    name: new FormControl("", [Validators.required]),
+    username: new FormControl("", [Validators.required]),
+    password: new FormControl("", [Validators.required]),
+  });
 
-  constructor() { }
+  constructor(
+    private httpService: CounterHttpService,
+    private dataService: CounterDataService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  createTraveler() {}
 }
