2a4e7d1da69a18d15e6b8e1dc0a2a147e745665c
Justin O'Brien <justin.obrien@smoothstack.com>
2020-07-01 20:38:34 -0600

    Counter: Make skeleton of select traveler form

--- src/app/counter/counter-select-traveler/counter-select-traveler.component.ts
+++ src/app/counter/counter-select-traveler/counter-select-traveler.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-counter-select-traveler',
-  templateUrl: './counter-select-traveler.component.html',
-  styleUrls: ['./counter-select-traveler.component.css']
+  selector: "app-counter-select-traveler",
+  templateUrl: "./counter-select-traveler.component.html",
+  styleUrls: ["./counter-select-traveler.component.css"],
 })
 export class CounterSelectTravelerComponent implements OnInit {
+  form = new FormGroup({
+    username: new FormControl("", [<any>Validators.required]),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
