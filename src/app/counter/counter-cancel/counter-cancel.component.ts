3b29e0268237167fa9ad04c705a821f0f3fde300
Justin O'Brien <justin.obrien@smoothstack.com>
2020-07-03 18:06:50 -0600

    Counter: Write constructor & on init for cancel component

--- src/app/counter/counter-cancel/counter-cancel.component.ts
+++ src/app/counter/counter-cancel/counter-cancel.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { Router } from "@angular/router";
+import { CounterHttpService } from "src/app/common/counter/service/counter-http.service";
+import { CounterDataService } from "src/app/common/counter/service/counter-data.service";
+import { environment } from "src/environments/environment";
 
 @Component({
-  selector: 'app-counter-cancel',
-  templateUrl: './counter-cancel.component.html',
-  styleUrls: ['./counter-cancel.component.css']
+  selector: "app-counter-cancel",
+  templateUrl: "./counter-cancel.component.html",
+  styleUrls: ["./counter-cancel.component.css"],
 })
 export class CounterCancelComponent implements OnInit {
+  traveler: any;
+  airports: any[];
+  flights: any[];
+  flight: any;
 
-  constructor() { }
+  constructor(
+    private modalService: NgbModal,
+    private router: Router,
+    private httpService: CounterHttpService,
+    private dataService: CounterDataService
+  ) {}
 
   ngOnInit() {
+    this.traveler = this.dataService.getTraveler();
+    if (!this.traveler) {
+      this.router.navigate(["/counter/traveler"]);
+      return;
+    }
+    this.dataService.travelerObservable.subscribe(
+      (traveler: any) => (this.traveler = traveler)
+    );
+    this.httpService
+      .get(environment.counterUrl + environment.counterAirportUri)
+      .subscribe(
+        (result: any[]) => (this.airports = result),
+        (error: any) =>
+          alert("Error getting airports: Status " + error.error.status)
+      );
   }
-
 }
