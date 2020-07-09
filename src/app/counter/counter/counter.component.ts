7c5b5cc073d6b1413e16da7eaac8212c381073d8
Justin O'Brien <justin.obrien@smoothstack.com>
2020-07-09 11:58:46 -0600

    Counter: Replace failed auth alert with toast

--- src/app/counter/counter/counter.component.ts
+++ src/app/counter/counter/counter.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { CounterAuthService } from "src/app/common/counter/service/counter-auth.service";
 import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
 import { CounterDataService } from "src/app/common/counter/service/counter-data.service";
+import { uncheckedErrorMessage } from "src/app/common/counter/counter-globals";
 
 @Component({
   selector: "app-counter",
@@ -15,6 +17,7 @@ export class CounterComponent implements OnInit {
 
   constructor(
     private router: Router,
+    private toastr: ToastrService,
     private dataService: CounterDataService,
     private authService: CounterAuthService
   ) {}
@@ -24,8 +27,11 @@ export class CounterComponent implements OnInit {
     this.authService.checkAuth().subscribe(
       () => (this.authorized = true),
       (error) => {
-        if (![401, 403].includes(error.error.status))
-          alert("Error checking authorization: Status " + error.error.status);
+        if (![401, 403, 500].includes(error.error.status))
+          this.toastr.error(
+            uncheckedErrorMessage,
+            "Error checking authorization: Status " + error.error.status
+          );
         this.router.navigate(["/counter/login"]);
       }
     );
