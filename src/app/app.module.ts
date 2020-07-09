bcd420a707685f8e4268398c40c4396846fa43cc
Justin O'Brien <justin.obrien@smoothstack.com>
2020-07-08 20:49:29 -0600

    Add ngx-toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import {
   FilterByDepartureDatePipe,
 } from "./common/h/filter-bookings";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ToastrModule } from 'ngx-toastr';
 import { CancelBookingComponent } from "./agent/agent-dashboard/cancel-booking/cancel-booking.component";
 import { CreateBookingComponent } from "./agent/agent-dashboard/create-booking/create-booking.component";
 import { SelectTravelerComponent } from "./agent/agent-dashboard/select-traveler/select-traveler.component";
@@ -77,6 +78,7 @@ import { TravelerLoginComponent } from './traveler/traveler-login/traveler-login
     ReactiveFormsModule,
     NgbModule,
     FormsModule,
+    ToastrModule.forRoot(),
     BrowserAnimationsModule,
     NgxStripeModule.forRoot(),
     NgMultiSelectDropDownModule,
