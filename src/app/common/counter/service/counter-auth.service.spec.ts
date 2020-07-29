4acb3d321978506902c84e9d43d332f3fdfe40ef
Justin O'Brien <justin.obrien@smoothstack.com>
2020-07-28 18:03:23 -0600

    Revert "Counter: Remove spec files for services not needing tests"
    The absence of the files caused errors to be printed to the console while
    running tests.

---
+++ src/app/common/counter/service/counter-auth.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { CounterAuthService } from './counter-auth.service';
+
+// describe('CounterAuthService', () => {
+//   beforeEach(() => TestBed.configureTestingModule({}));
+
+//   it('should be created', () => {
+//     const service: CounterAuthService = TestBed.get(CounterAuthService);
+//     expect(service).toBeTruthy();
+//   });
+// });
---
+++ src/app/common/counter/service/counter-http.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { CounterHttpService } from './counter-http.service';
+
+// describe('CounterHttpService', () => {
+//   beforeEach(() => TestBed.configureTestingModule({}));
+
+//   it('should be created', () => {
+//     const service: CounterHttpService = TestBed.get(CounterHttpService);
+//     expect(service).toBeTruthy();
+//   });
+// });
