abb4dfe5e5821f13aac22e92aa32dd11d9897006
Justin O'Brien <justin.obrien@smoothstack.com>
2020-08-24 17:42:17 -0600

    Write before each function for counter interception service tests

--- src/app/common/counter/service/counter-interception.service.spec.ts
+++ src/app/common/counter/service/counter-interception.service.spec.ts
@@ -1,12 +1,39 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
 
-import { CounterInterceptionService } from './counter-interception.service';
+import { CounterInterceptionService } from "./counter-interception.service";
+import { CounterHttpService } from "./counter-http.service";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
 
-describe('CounterInterceptionService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe("CounterInterceptionService", () => {
+  let service: CounterHttpService;
+  let mockHttp: HttpTestingController;
 
-  it('should be created', () => {
-    const service: CounterInterceptionService = TestBed.get(CounterInterceptionService);
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        CounterHttpService,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: CounterInterceptionService,
+          multi: true,
+        },
+      ],
+    });
+    service = TestBed.get(CounterHttpService);
+    mockHttp = TestBed.get(HttpTestingController);
+  });
+
+  it("should be created", () => {
+    const service: CounterInterceptionService = TestBed.get(
+      CounterInterceptionService
+    );
     expect(service).toBeTruthy();
   });
+
+  
 });
