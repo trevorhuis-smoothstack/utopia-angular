0d28fa535d1ea4c665267d050d21a3dd1071cbab
Justin O'Brien <justin.obrien@smoothstack.com>
2020-08-25 17:36:21 -0600

    Counter: Get root create test passing
    All tests pass but errors are thrown while testing;
    "Cannot match any routes."

--- src/app/counter/counter/counter.component.spec.ts
+++ src/app/counter/counter/counter.component.spec.ts
@@ -1,27 +1,34 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { CounterComponent } from './counter.component';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CounterComponent } from "./counter.component";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { ToastrModule } from "ngx-toastr";
+import { HttpClientModule } from '@angular/common/http';
 
-// describe('CounterComponent', () => {
-//   let component: CounterComponent;
-//   let fixture: ComponentFixture<CounterComponent>;
+describe("CounterComponent", () => {
+  let component: CounterComponent;
+  let fixture: ComponentFixture<CounterComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-//       declarations: [ CounterComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      declarations: [CounterComponent],
+      imports: [
+        HttpClientModule,
+        RouterModule.forRoot([]),
+        ToastrModule.forRoot(),
+      ],
+    }).compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(CounterComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CounterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
