455c3f9be252fb523d4e00a5b630a3938bcc918c
Justin O'Brien <justin.obrien@smoothstack.com>
2020-08-24 18:06:47 -0600

    Counter: Write 100% test of interception service

--- src/app/common/counter/service/counter-interception.service.spec.ts
+++ src/app/common/counter/service/counter-interception.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from "@angular/core/testing";
 import {
   HttpClientTestingModule,
   HttpTestingController,
+  TestRequest,
 } from "@angular/common/http/testing";
 
 import { CounterInterceptionService } from "./counter-interception.service";
@@ -35,5 +36,18 @@ describe("CounterInterceptionService", () => {
     expect(service).toBeTruthy();
   });
 
-  
+  it("should add a an authorization heder with a stored token", () => {
+    const token = "mockToken";
+    let request: TestRequest;
+    localStorage.setItem("token", token);
+    service.get("").subscribe();
+    request = mockHttp.expectOne("");
+    expect(request.request.headers.get("Authorization")).toBe(
+      "Bearer " + token
+    );
+    localStorage.removeItem("token");
+    service.get("").subscribe();
+    request = mockHttp.expectOne("");
+    expect(request.request.headers.has("Authorization")).toBe(false);
+  });
 });
