c422bdb32f3cfd0bdd3e6aa4dc64a6889bbb6a57
Justin O'Brien <justin.obrien@smoothstack.com>
2020-08-25 16:54:56 -0600

    Counter: Write 100% tests of data service

--- src/app/common/counter/service/counter-data.service.spec.ts
+++ src/app/common/counter/service/counter-data.service.spec.ts
@@ -1,12 +1,48 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { CounterDataService } from './counter-data.service';
+import { CounterDataService } from "./counter-data.service";
 
-describe('CounterDataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe("CounterDataService", () => {
+  let service: CounterDataService;
 
-  it('should be created', () => {
-    const service: CounterDataService = TestBed.get(CounterDataService);
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.get(CounterDataService);
+  });
+
+  it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  it("should set & get the counter attendant", () => {
+    const mockCounter = {
+      userId: 1,
+      name: "Mock Counter",
+      username: "MockCounter",
+      password: null,
+      role: "COUNTER",
+    };
+    expect(service.getCounter()).toBeFalsy();
+    service.setCounter(mockCounter);
+    expect(service.getCounter()).toEqual(mockCounter);
+  });
+
+  it("should set & get the traveler", () => {
+    const mockTraveler = {
+      userId: 1,
+      name: "Mock Traveler",
+      username: "MockTraveler",
+      password: null,
+      role: "TRAVELER",
+    };
+    let traveler: any;
+    service.travelerObservable.subscribe(
+      (thisTraveler) => (traveler = thisTraveler)
+    );
+    expect(traveler).toBeFalsy()
+    expect(service.getTraveler()).toBeFalsy();
+    service.setTraveler(mockTraveler);
+    expect(service.getTraveler()).toEqual(mockTraveler);
+    expect(traveler).toEqual(mockTraveler);
   });
 });
