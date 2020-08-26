371b310e1308476ace00ead032841a7713790a62
Justin O'Brien <justin.obrien@smoothstack.com>
2020-08-26 12:13:37 -0600

    Counter: Import mock data in data service tests

--- src/app/common/counter/service/counter-data.service.spec.ts
+++ src/app/common/counter/service/counter-data.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from "@angular/core/testing";
 
 import { CounterDataService } from "./counter-data.service";
+import{mockCounter, mockTraveler} from "src/app/common/counter/counter-mock-data"
 
 describe("CounterDataService", () => {
   let service: CounterDataService;
@@ -15,26 +16,12 @@ describe("CounterDataService", () => {
   });
 
   it("should set & get the counter attendant", () => {
-    const mockCounter = {
-      userId: 1,
-      name: "Mock Counter",
-      username: "MockCounter",
-      password: null,
-      role: "COUNTER",
-    };
     expect(service.getCounter()).toBeFalsy();
     service.setCounter(mockCounter);
     expect(service.getCounter()).toEqual(mockCounter);
   });
 
   it("should set & get the traveler", () => {
-    const mockTraveler = {
-      userId: 1,
-      name: "Mock Traveler",
-      username: "MockTraveler",
-      password: null,
-      role: "TRAVELER",
-    };
     let traveler: any;
     service.travelerObservable.subscribe(
       (thisTraveler) => (traveler = thisTraveler)
