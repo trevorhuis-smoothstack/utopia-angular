53f1c4442c8a5fa19a21cf593bcd57032b7c1ca1
Justin O'Brien <justin.obrien@smoothstack.com>
2020-08-27 17:58:33 -0600

    Counter: Get test of select traveler component creation working

--- src/app/counter/counter-select-traveler/counter-select-traveler.component.spec.ts
+++ src/app/counter/counter-select-traveler/counter-select-traveler.component.spec.ts
@@ -1,27 +1,36 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { CounterSelectTravelerComponent } from './counter-select-traveler.component';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CounterSelectTravelerComponent } from "./counter-select-traveler.component";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterTestingModule } from "@angular/router/testing";
+import { ToastrService, ToastrModule } from "ngx-toastr";
 
-// describe('CounterSelectTravelerComponent', () => {
-//   let component: CounterSelectTravelerComponent;
-//   let fixture: ComponentFixture<CounterSelectTravelerComponent>;
+describe("CounterSelectTravelerComponent", () => {
+  let component: CounterSelectTravelerComponent;
+  let fixture: ComponentFixture<CounterSelectTravelerComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-//       declarations: [ CounterSelectTravelerComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      declarations: [CounterSelectTravelerComponent],
+      imports: [
+        ReactiveFormsModule,
+        HttpClientModule,
+        RouterTestingModule.withRoutes([{ path: "**", redirectTo: "" }]),
+        ToastrModule.forRoot(),
+      ],
+    }).compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(CounterSelectTravelerComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CounterSelectTravelerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
