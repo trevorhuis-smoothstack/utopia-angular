5fa93c0c476030d6f0701763746c456e0a4a3dc0
Justin O'Brien <justin.obrien@smoothstack.com>
2020-08-28 16:48:51 -0600

    Counter: Remove unused imports from select traveler component spec

--- src/app/counter/counter-select-traveler/counter-select-traveler.component.spec.ts
+++ src/app/counter/counter-select-traveler/counter-select-traveler.component.spec.ts
@@ -1,18 +1,8 @@
-import {
-  async,
-  ComponentFixture,
-  TestBed,
-  fakeAsync,
-  tick,
-} from "@angular/core/testing";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { CounterSelectTravelerComponent } from "./counter-select-traveler.component";
 import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
-import {
-  ReactiveFormsModule,
-  AbstractControl,
-  FormControl,
-} from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { RouterTestingModule } from "@angular/router/testing";
 import { ToastrService, ToastrModule } from "ngx-toastr";
@@ -20,7 +10,6 @@ import { Router } from "@angular/router";
 import { CounterHttpService } from "src/app/common/counter/service/counter-http.service";
 import { CounterDataService } from "src/app/common/counter/service/counter-data.service";
 import {
-  mockUsername,
   mockControl,
   mockTraveler,
 } from "src/app/common/counter/counter-mock-data";
