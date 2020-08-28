c8666bf0475de2b269172f52ba31475f17515e0b
Justin O'Brien <justin.obrien@smoothstack.com>
2020-08-28 17:42:34 -0600

    Counter: Test password match validation function of create traveler component

--- src/app/counter/counter-create-traveler/counter-create-traveler.component.spec.ts
+++ src/app/counter/counter-create-traveler/counter-create-traveler.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { CounterCreateTravelerComponent } from "./counter-create-traveler.component";
-import { ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule, FormGroup, FormControl } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { RouterTestingModule } from "@angular/router/testing";
 import { Router } from "@angular/router";
@@ -102,5 +102,20 @@ describe("CounterCreateTravelerComponent", () => {
         environment.counterUsernameUri +
         mockControl.value
     );
+  });
+
+  it("should correctly discern whether the passwords match", () => {
+    const mockValidForm = new FormGroup({
+        password: new FormControl("x"),
+        confirmPassword: new FormControl("x"),
+      }),
+      mockInvalidForm = new FormGroup({
+        password: new FormControl("x"),
+        confirmPassword: new FormControl("y"),
+      });
+    expect(component.validatePasswordMatch(mockValidForm)).toBeNull();
+    expect(component.validatePasswordMatch(mockInvalidForm)).toEqual({
+      validatePasswordMatch: true,
+    });
   });
 });
