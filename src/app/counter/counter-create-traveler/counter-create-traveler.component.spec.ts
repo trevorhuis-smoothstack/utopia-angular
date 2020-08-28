ccc47c92f67335343ad0eb60c50c7da6f4e2b409
Justin O'Brien <justin.obrien@smoothstack.com>
2020-08-28 16:52:20 -0600

    Counter: Make skeleton of spec of create traveler component

--- src/app/counter/counter-create-traveler/counter-create-traveler.component.spec.ts
+++ src/app/counter/counter-create-traveler/counter-create-traveler.component.spec.ts
@@ -1,25 +1,51 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { CounterCreateTravelerComponent } from './counter-create-traveler.component';
+import { CounterCreateTravelerComponent } from "./counter-create-traveler.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterTestingModule } from "@angular/router/testing";
+import { Router } from "@angular/router";
+import { ToastrService, ToastrModule } from "ngx-toastr";
+import { CounterHttpService } from "src/app/common/counter/service/counter-http.service";
+import { CounterDataService } from "src/app/common/counter/service/counter-data.service";
 
-// describe('CounterCreateTravelerComponent', () => {
-//   let component: CounterCreateTravelerComponent;
-//   let fixture: ComponentFixture<CounterCreateTravelerComponent>;
+describe("CounterCreateTravelerComponent", () => {
+  let component: CounterCreateTravelerComponent;
+  let fixture: ComponentFixture<CounterCreateTravelerComponent>;
+  let router: Router,
+    toastr: ToastrService,
+    httpService: CounterHttpService,
+    dataService: CounterDataService;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ CounterCreateTravelerComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [CounterCreateTravelerComponent],
+      imports: [
+        ReactiveFormsModule,
+        HttpClientModule,
+        RouterTestingModule.withRoutes([{ path: "**", redirectTo: "" }]),
+        ToastrModule.forRoot(),
+      ],
+    }).compileComponents();
+    toastr = TestBed.get(ToastrService);
+    router = TestBed.get(Router);
+    httpService = TestBed.get(CounterHttpService);
+    dataService = TestBed.get(CounterDataService);
+    component = new CounterCreateTravelerComponent(
+      toastr,
+      router,
+      httpService,
+      dataService
+    );
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(CounterCreateTravelerComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CounterCreateTravelerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
