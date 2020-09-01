e09a75c98eba41f96c299448667207993811e150
Justin O'Brien <justin.obrien@smoothstack.com>
2020-08-31 18:03:38 -0600

    Counter: Get creation test of cancellation component working

--- src/app/counter/counter-cancellation/counter-cancellation.component.spec.ts
+++ src/app/counter/counter-cancellation/counter-cancellation.component.spec.ts
@@ -1,27 +1,67 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { CounterCancellationComponent } from './counter-cancellation.component';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CounterCancellationComponent } from "./counter-cancellation.component";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterTestingModule } from "@angular/router/testing";
+import { ToastrModule, ToastrService } from "ngx-toastr";
+import { NgbModal, NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { Router } from "@angular/router";
+import { CounterHttpService } from "src/app/common/counter/service/counter-http.service";
+import { CounterDataService } from "src/app/common/counter/service/counter-data.service";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { CounterAirportFilterPipe } from "src/app/common/counter/pipe/counter-airport-filter.pipe";
+import { CounterDateFilterPipe } from "src/app/common/counter/pipe/counter-date-filter.pipe";
 
-// describe('CounterCancellationComponent', () => {
-//   let component: CounterCancellationComponent;
-//   let fixture: ComponentFixture<CounterCancellationComponent>;
+describe("CounterCancellationComponent", () => {
+  let component: CounterCancellationComponent;
+  let fixture: ComponentFixture<CounterCancellationComponent>;
+  let router: Router,
+    toastr: ToastrService,
+    modalService: NgbModal,
+    httpService: CounterHttpService,
+    dataService: CounterDataService;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-//       declarations: [ CounterCancellationComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      declarations: [
+        CounterCancellationComponent,
+        CounterAirportFilterPipe,
+        CounterDateFilterPipe,
+      ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        RouterTestingModule.withRoutes([{ path: "**", redirectTo: "" }]),
+        NgbModule,
+        ToastrModule.forRoot(),
+      ],
+    }).compileComponents();
+    toastr = TestBed.get(ToastrService);
+    router = TestBed.get(Router);
+    modalService = TestBed.get(NgbModal);
+    httpService = TestBed.get(CounterHttpService);
+    dataService = TestBed.get(CounterDataService);
+    component = new CounterCancellationComponent(
+      modalService,
+      router,
+      toastr,
+      httpService,
+      dataService
+    );
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(CounterCancellationComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    spyOn(document, "getElementById").and.returnValue(
+      document.createElement("li")
+    );
+    fixture = TestBed.createComponent(CounterCancellationComponent);
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
